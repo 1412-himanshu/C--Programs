a71214c275664cc784d6f95400013c91d18a085d
Himanshu D Hatwar <hatwarhimanshu1412@gmail.com>
2022-02-27 11:23:32 +0530

    Division Program

---
+++ Division.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float num1,num2,result;
+    cout<<"Division of Numbers"<<endl;
+    cout<<"Enter first number"<<endl;
+    cin>>num1;
+    cout<<"Enter second number"<<endl;
+    cin>>num2;
+    result = num1 / num2;
+    cout<<"The division of the given numbers is : "<<result<<endl;
+    return 0;
+}
