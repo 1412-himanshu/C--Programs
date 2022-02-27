47c7d109c06f646fb4dd0276aa855d4347584a3e
Himanshu D Hatwar <hatwarhimanshu1412@gmail.com>
2022-02-27 11:27:24 +0530

    Division Program

--- Division.cpp
+++ Division.cpp
@@ -2,13 +2,18 @@
 using namespace std;
 int main()
 {
-    float num1,num2,result;
-    cout<<"Division of Numbers"<<endl;
-    cout<<"Enter first number"<<endl;
-    cin>>num1;
-    cout<<"Enter second number"<<endl;
-    cin>>num2;
-    result = num1 / num2;
-    cout<<"The division of the given numbers is : "<<result<<endl;
+    float num1, num2, result;                                                               // Initializing the variables
+    
+    cout << "Division of Numbers" << endl;                                                  // Print message
+    
+    cout << "Enter first number" << endl;
+    cin >> num1;                                                                            // Input first number
+    
+    cout << "Enter second number" << endl;
+    cin >> num2;                                                                            // Input second number
+    
+    result = num1 / num2;                                                                   // Division Operation
+    
+    cout << "The division of the given numbers is : " << result << endl;                    // Result Generation           
     return 0;
 }
